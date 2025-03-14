ba84b143d875134fe2cc832a0eb8f8b8e9e4ca2b
Yukicorn <monique.rausche@gmail.com>
2025-03-14 15:15:31 +0100

    Notes

---
+++ src/main/java/de/thowl/prog3/exam/storage/entities/Notes.java
@@ -0,0 +1,32 @@
+package de.thowl.prog3.exam.storage.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "notes")
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY ) //erzeuge ID automatisch
+    private Long id;
+
+    @Column(nullable = false) // darf nicht leer sein
+    private String title;
+
+    @Column(columnDefinition = "TEXT") //TEXT kann längere Texte speichern als Varchar sinnvoll?
+    private String content;
+    private String tags;
+    private LocalDateTime createdAt = LocalDateTime.now(); // speichere datum, uhrzeit notiz erstellt
+}
